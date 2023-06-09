5a4273cb0fc467e6898c435c265d3eec9c539a14
Guns <gunjan.soni2002@gmail.com>
2023-06-09 18:36:03 +0200

    updates queue class to be more performant

--- src/queue.ts
+++ src/queue.ts
@@ -1,5 +1,6 @@
-import { EventEmitter } from 'events'
-import { getCpuUsage, getMemoryUsage } from './helpers'
+import * as os from 'os'
+
+type Task = () => Promise<any>;
 
 export type QueueOptions = {
   maxConcurrency: number
@@ -8,165 +9,111 @@ export type QueueOptions = {
   initialConcurrency: number
 }
 
-
-export class Queue extends EventEmitter {
-  private _queue: Array<{ cb: () => Promise<unknown>, resolve: (data: unknown) => void, reject: (err: Error) => void }> = []
-  private pending: Array<{ cb: () => Promise<unknown>, resolve: (data: unknown) => void, reject: (err: Error) => void }> = []
-  private _isProcessing = false
-  private _isPaused = false
-  private readonly options: QueueOptions
-  private currentCpuUsage: number
-  private currentMemoryUsage: number
+export class Queue {
+  private cpuThreshold: number
+  private memoryThreshold: number
   private concurrency: number
-  private iterations: number = 0
+  private maxConcurrency: number
+  private queue: Task[]
+  private running: number
+  private interval: NodeJS.Timeout | null
+  private checkInterval: number = 10000
 
   constructor(opts: Partial<QueueOptions> = {}) {
-    super()
-    this.options = {
-      maxCpuUsage: 0.8,
-      maxMemoryUsage: 0.8,
-      initialConcurrency: 1,
-      maxConcurrency: Infinity,
-      ...opts,
-    } as QueueOptions
-    this.currentCpuUsage = getCpuUsage()
-    this.currentMemoryUsage = getMemoryUsage()
-    this.concurrency = this.options.initialConcurrency
-    this.on('process', this.onProcess.bind(this))
+    this.cpuThreshold = opts.maxCpuUsage || 0.8
+    this.memoryThreshold = opts.maxMemoryUsage || 0.8
+    this.concurrency = opts.initialConcurrency || 1
+    this.maxConcurrency = opts.maxConcurrency || Infinity
+    this.queue = []
+    this.running = 0
+    this.interval = null
+    this.startSystemStatsCheck(this.checkInterval)
   }
 
-  private refactorConcurrency(): number {
-    if (this.iterations % 5 === 0) {
-      const memoryUsageDiff = this.memoryUsageDiff
-      const cpuUsageDiff = this.cpuUsageDiff
-      const { maxConcurrency, maxMemoryUsage, maxCpuUsage } = this.options
-      if (this.memoryUsage >= maxMemoryUsage * 0.9 || this.cpuUsage >= maxCpuUsage * 0.9) {
-        this.concurrency = Math.max(this.concurrency - 1, 1)
-        return this.concurrency
-      }
-
-      if (this.concurrency >= maxConcurrency) {
-        this.concurrency = maxConcurrency
-        return this.concurrency
-      }
-
-      if (memoryUsageDiff <= 0) {
-        this.concurrency = Math.min(this.concurrency + 2, maxConcurrency)
-      }
-
-      if (cpuUsageDiff <= 0) {
-        this.concurrency = Math.min(this.concurrency + 2, maxConcurrency)
-      }
-
-      if (memoryUsageDiff <= 0.1) {
-        this.concurrency = Math.min(this.concurrency + 1, maxConcurrency)
-      }
-
-      if (cpuUsageDiff <= 0.1) {
-        this.concurrency = Math.min(this.concurrency + 1, maxConcurrency)
-      }
-    }
-    return this.concurrency
-
-  }
-
-  get currentConcurrency() {
-    return this.concurrency
-  }
-
-  get cpuUsage() {
-    return getCpuUsage()
-  }
-
-  get memoryUsage() {
-    return getMemoryUsage()
-  }
-
-  get cpuUsageDiff() {
-    const prevCpuUsage = this.currentCpuUsage
-    this.currentCpuUsage = getCpuUsage()
-    return this.currentCpuUsage - prevCpuUsage
-  }
-
-  get memoryUsageDiff() {
-    const prevMemoryUsage = this.currentMemoryUsage
-    this.currentMemoryUsage = getMemoryUsage()
-    return this.currentMemoryUsage - prevMemoryUsage
-  }
-
-  get length() {
-    return this._queue.length
-  }
-
-  get isProcessing() {
-    return this._isProcessing
-  }
-
-  get isPaused() {
-    return this._isPaused
-  }
-
-  private async sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
-
-  async waitUntilReady(): Promise<boolean> {
-    if (this._queue.length === 0 || this.pending.length < this.concurrency) {
-      return true
-    }
-    await this.sleep(100)
-    return this.waitUntilReady()
-  }
-
-  async done() {
-    while (this._queue.length > 0 || this.pending.length > 0) {
-      await this.sleep(100)
-    }
-  }
-
-  async enqueue(cb: () => Promise<unknown>): Promise<{ promise: Promise<unknown> }> {
-    await this.waitUntilReady()
-    const promise = new Promise((resolve, reject) => {
-      this._queue.push({ cb, resolve, reject })
-      this.emit('added')
-      this._processQueue()
-    })
+  // Push a new Task into the Queue
+  enqueue(task: Task) {
+    const promise = () => new Promise((resolve, reject) => {
+      task()
+        .then(resolve)
+        .catch(reject)
+    }).finally(() => { this.next() })
+    this.queue.push(promise)
+    this.next()
     return { promise }
   }
 
-  private dequeue() {
-    return this._queue.shift()
-  }
-
-  pause() {
-    this._isPaused = true
-  }
-
-  resume() {
-    this._isPaused = false
-    this._processQueue()
-  }
-
-  clear() {
-    this._queue = []
-  }
-
-  onProcess(item: { cb: () => Promise<unknown>, resolve: (data: unknown) => void, reject: (err: Error) => void }) {
-    this.pending.push(item)
-    this.iterations++
-    item.cb().then(item.resolve).catch(item.reject).finally(() => {
-      this.pending = this.pending.filter(p => p !== item)
-      this._processQueue()
+  async done() {
+    await new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        if (this.queue.length === 0 && this.running === 0) {
+          clearInterval(interval)
+          resolve(true)
+        }
+      }, 100)
     })
   }
 
-  private _processQueue() {
+  // Run the next Task
+  private next(): void {
+    if (this.running >= this.concurrency || this.queue.length === 0) {
+      return
+    }
 
-    this.concurrency = this.refactorConcurrency()
+    this.running++
+    const task = this.queue.shift()
+    if (task) {
+      task()
+        .then(() => {
+          this.running--
+          this.next()
+        })
+        .catch((err) => {
+          this.running--
+          this.next()
+        })
+    }
+    this.next()
+  }
 
-    if (this._isPaused || this._queue.length === 0 || this.pending.length >= this.concurrency) return
-    const item = this.dequeue()
-    this.emit('process', item)
-    this._processQueue()
+  // Function to check system stats and adjust concurrency
+  private checkSystemStats(): void {
+    const freeMemPercentage = (os.freemem() / os.totalmem()) * 100
+    const cpuUsage = (os.loadavg()[0] / os.cpus().length) // 1 minute load average
+    const memFactor = freeMemPercentage > this.memoryThreshold ? ((freeMemPercentage - this.memoryThreshold) / 100) : 0
+    const cpuFactor = cpuUsage < this.cpuThreshold ? ((this.cpuThreshold - cpuUsage) / this.cpuThreshold) : 0
+    const increaseFactor = Math.min(memFactor, cpuFactor)
+    const decreaseFactor = 1 - increaseFactor;
+
+    console.log(`Free memory: ${freeMemPercentage}%`, this.memoryThreshold, freeMemPercentage > this.memoryThreshold)
+    console.log(`CPU usage: ${cpuUsage}%`, this.cpuThreshold, cpuUsage < this.cpuThreshold)
+    console.log(`Concurrency: ${this.concurrency}`)
+
+
+    if (increaseFactor > 0) {
+        this.concurrency = Math.ceil(this.concurrency * (1 + increaseFactor));
+        console.log(`Increasing concurrency to ${this.concurrency}`)
+    } else if (decreaseFactor > 0) {
+        this.concurrency = Math.max(1, Math.floor(this.concurrency * (1 - decreaseFactor)));
+        console.log(`Decreasing concurrency to ${this.concurrency}`)
+    }
+  }
+
+  // Function to start system stats check in intervals
+  startSystemStatsCheck(interval: number): void {
+    if (this.interval) {
+      clearInterval(this.interval)
+    }
+    this.interval = setInterval(() => {
+      this.checkSystemStats()
+      this.next()  // Trigger the queue processing after adjusting the concurrency
+    }, interval)
+  }
+
+  // Function to stop system stats check
+  stopSystemStatsCheck(): void {
+    if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = null
+    }
   }
 }
